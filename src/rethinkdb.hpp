6b1cc9db3480ce631a11e6dae47c60cf0c052ce1
Chip Collier <ccollier@machinezone.com>
2016-02-08 17:17:54 +0100

    Added essential install targets

---
+++ src/rethinkdb.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "config.h"
+
+#include "utils.h"
+#include "types.h"
+#include "query.h"
+#include "net.h"
+#include "json.h"
+#include "datum.h"
+#include "cursor.h"
+#include "stream.h"
+#include "error.h"
+
+#include "protocol_defs.h"
